f80ff2e5d73b45cfbaa02c8df4c63a67c4bc48f9
Shishkin <ilshishkin@gmail.com>
2019-09-28 19:48:41 +0300

    Add tests, add some checks

--- tests/lib/rules/map.js
+++ tests/lib/rules/map.js
@@ -17,20 +17,27 @@ var rule = require("../../../lib/rules/map"),
 // Tests
 //------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester();
+var ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2015 } });
 ruleTester.run("map", rule, {
 
     valid: [
-
         // give me some code that won't trigger a warning
+        "_.map({ a: 1, b: 2 }, item => ++item)",
+        "_.map({ a: 1, b: 2 }, fn)",
+        "Array.isArray(collection) ? collection.map(fn) : _.map(collection, fn)"
     ],
 
     invalid: [
         {
-            code: "",
+            code: "_.map([1, 2, 3], fn)",
             errors: [{
-                message: "Fill me in.",
-                type: "Me too"
+                message: "Consider to use native javascript map implementation"
+            }]
+        },
+        {
+            code: "_.map(collection, fn)",
+            errors: [{
+                message: "Consider to use native javascript map implementation"
             }]
         }
     ]
